f5b4bf14648c3d30507c06ba4fee42067d604915
anala <analampr1@gmail.com>
2019-05-09 18:18:34 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	README.md

--- Programming_Part/read_image.py
+++ Programming_Part/read_image.py
@@ -3,38 +3,48 @@ import numpy as np
 import matplotlib.pyplot as plt
 import glob, os
 import torchvision
+import torch
 import tqdm
 
 dir_path = "C:/Users/anala/Desktop/coursecontent/Deep Learning/Project/Our-Lovely-Awesome-Team-Project/CelebA/img_celeba/data_crop_512_png"
-size = 512, 512
-counter = 1
-# for filename in os.listdir(dir_path):
-#     if filename.endswith(".jpg"):
-#         im = Image.open(dir_path + "/" + filename)
-#         filename = filename.split(".")[0]
-#         filename = filename + ".png"
-#         im.save(filename)
-#         counter +=1
-#         print(counter)
-#     if counter > 50:
-#         break
+dir_path2 = "C:/Users/anala/Desktop/coursecontent/Deep Learning/Project/Our-Lovely-Awesome-Team-Project/CelebA/img_celeba/data_crop_512_png/masks"
+masks = {}
+"""THE MASKS"""
+for infile in glob.glob(dir_path2 + "/*.png"):
+    file, ext = os.path.splitext(infile)
+    im = Image.open(infile)
+    trans = torchvision.transforms.ToPILImage()
+    trans2 = torchvision.transforms.ToTensor()
+    alpha_im = np.array(trans(trans2(im)).split()[3])
+    alpha_im[alpha_im != 255] = 0
+    name = infile.split('/')[-1]
+    masks[name] = trans(trans2(alpha_im))
 
+key_array = np.array(list(masks.keys()))
+dest_tensor_list = []
+tensor_list = []
+counter = 0
+"""SAVES THE IMAGES AND THE DESTROYED ONES AS TWO BIG TENSORS"""
 for infile in glob.glob(dir_path + "/*.png"):
     file, ext = os.path.splitext(infile)
     im = Image.open(infile)
     trans = torchvision.transforms.ToPILImage()
-
     trans2 = torchvision.transforms.ToTensor()
     plt.imshow(trans(trans2(im)))
     tensor_im = trans2(im)
     pil_im = trans(trans2(im))
-    alpha_im = np.array(trans(trans2(im)).split()[3])
-    alpha_im[alpha_im != 255] = 0
-    destroyed_im = np.array(pil_im)
-    destroyed_im[:,:,3] = alpha_im
-    print('hi')
-
-
-print('hi')
-
-
+    pil_im_dest = trans(trans2(im))
+    key = np.random.choice(key_array)
+    pil_im_dest.putalpha(masks[key])
+    tensor_im_dest = trans2(pil_im_dest)
+    plt.imshow(trans(trans2(pil_im_dest)))
+    dest_tensor_list.append(tensor_im_dest)
+    tensor_list.append(tensor_im)
+    counter += 1
+    """STOP AT 10 IMAGES"""
+    if counter == 10:
+        break
+dest_data = torch.stack(dest_tensor_list)
+data = torch.stack(tensor_list)
+torch.save(dest_data, 'dest_data.pt')
+torch.save(data, 'data.pt')
