a15fd698bf2393a6b685160624b33d432e3334f0
anala <analampr1@gmail.com>
2019-05-12 15:28:41 +0200

    Updated Data

--- Programming_Part/read_image.py
+++ Programming_Part/read_image.py
@@ -23,28 +23,44 @@ for infile in glob.glob(dir_path2 + "/*.png"):
 key_array = np.array(list(masks.keys()))
 dest_tensor_list = []
 tensor_list = []
+total_dest_tensor_list = []
 counter = 0
 """SAVES THE IMAGES AND THE DESTROYED ONES AS TWO BIG TENSORS"""
 for infile in glob.glob(dir_path + "/*.png"):
     file, ext = os.path.splitext(infile)
+    """READS THE IMAGE"""
     im = Image.open(infile)
+    """PIL TRANSFORMATION"""
     trans = torchvision.transforms.ToPILImage()
+    """TENSOR TRANSFORMATION"""
     trans2 = torchvision.transforms.ToTensor()
-    plt.imshow(trans(trans2(im)))
     tensor_im = trans2(im)
     pil_im = trans(trans2(im))
     pil_im_dest = trans(trans2(im))
+    """GET A RANDOM DESTRUCTION PATTERN"""
     key = np.random.choice(key_array)
+    mask = np.array(masks[key])
+    """MAKE IT 1/0"""
+    mask[mask != 0] = 1
+    """CREATE AN IMAGE WITH ALL RGB CHANNELS AFFECTED"""
+    pil_im_total_dest = trans(np.multiply(np.array(pil_im_dest), mask[:, :, None]))
+    pil_im_total_dest.putalpha(masks[key])
+    """CREATE AN IMAGE WITH ONLY THE ALPHA CHANNEL DESTROYED"""
     pil_im_dest.putalpha(masks[key])
+    tensor_im_total_dest = trans2(pil_im_total_dest)
     tensor_im_dest = trans2(pil_im_dest)
-    plt.imshow(trans(trans2(pil_im_dest)))
     dest_tensor_list.append(tensor_im_dest)
     tensor_list.append(tensor_im)
+    total_dest_tensor_list.append(tensor_im_total_dest)
     counter += 1
     """STOP AT 10 IMAGES"""
     if counter == 10:
         break
+"""STACK THE DATA IN (NUMBER x CHANNELS x PIXELS x PIXELS)"""
 dest_data = torch.stack(dest_tensor_list)
 data = torch.stack(tensor_list)
+total_dest_data = torch.stack(total_dest_tensor_list)
+"""SAVE THE DATA IN PT FORMAT"""
 torch.save(dest_data, 'dest_data.pt')
 torch.save(data, 'data.pt')
+torch.save(total_dest_data, 'total_dest_data.pt')
