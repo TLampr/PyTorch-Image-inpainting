7e836107f01d72d9a8a5659377627e5ac818fa12
Azilyss <49938372+Azilyss@users.noreply.github.com>
2019-05-19 22:57:32 +0200

    Update metrics.py

--- metrics.py
+++ metrics.py
@@ -21,14 +21,15 @@ def compute_MSSIM(y_true, y_pred, window_size) :
     MSSIM[i] /= y_true.shape[1] #average over channels
   return MSSIM
 
-y_true = torch.rand(10, 3, 512, 512)
-y_pred = torch.rand(10, 3, 512, 512)
+if __name__ == '__main__':
+  y_true = torch.rand(10, 3, 512, 512)
+  y_pred = torch.rand(10, 3, 512, 512)
 
-y_true = y_true.numpy()
-y_pred = y_pred.numpy()
+  y_true = y_true.numpy()
+  y_pred = y_pred.numpy()
 
-print("PSNR : ", PSNR(y_true, y_pred))
-print("l1 : ", l1(y_true, y_pred))
+  print("PSNR : ", PSNR(y_true, y_pred))
+  print("l1 : ", l1(y_true, y_pred))
 
-window_size = 7
-print("MSSIM average over nb of channels :", compute_MSSIM(y_true, y_pred, window_size))
+  window_size = 7
+  print("MSSIM average over nb of channels :", compute_MSSIM(y_true, y_pred, window_size))
