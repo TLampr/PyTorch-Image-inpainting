27aed9106fe28bacae2e888fe2c78ea665578bb1
Manon Deprette <manon.deprette@gmail.com>
2019-05-23 18:14:12 +0200

    r

--- Programming_Part/loss.py
+++ Programming_Part/loss.py
@@ -1,3 +1,10 @@
+import torch
+from torch import nn
+from torch.autograd import Variable
+from torchvision import models
+
+is_cuda = torch.cuda.is_available()
+
 class loss(nn.modules.loss._WeightedLoss):
     def __init__(self):
         super(loss, self).__init__()
@@ -16,30 +23,19 @@ class loss(nn.modules.loss._WeightedLoss):
         self.Iout = Iout
         self.mask = mask
         self.make_comp()
+        
         return self.loss_function()
 
     def make_comp(self):
-        mask = self.mask.clone().type(torch.ByteTensor).cuda()
+        mask = self.mask.clone().type(torch.ByteTensor)
+        if is_cuda :
+            mask = mask.cuda() 
+            
         self.Icomp = Variable(torch.where(mask, self.Igt, self.Iout), requires_grad = True)
 
-        """mask = np.ones(X.shape, dtype=bool)
-        mask[:, validation_ind] = 0
-
-        X_train = X[mask].reshape(d*n_len, -1)"""
-
     def loss_function(self):
-       
         loss_total = 6 * self.l_hole() + self.l_valid() + 0.05 * self.l_perc()\
                     + 120 * (self.l_style_comp() + self.l_style_out()) + 0.1 * self.l_tv()
-        
-        #print("hole : ", self.l_hole())
-        #print("valid : ", self.l_valid())
-        #print("perc : ", self.l_perc())
-
-        #print("style comp : ", self.l_style_comp())
-        #print("style out : ", self.l_style_out())
-
-        #print("tv : ", self.l_tv())
 
         self.Igt = None
         self.Iout = None
@@ -49,7 +45,8 @@ class loss(nn.modules.loss._WeightedLoss):
         self.pool_gt = None
         self.pool_comp = None
         
-        torch.cuda.empty_cache()
+        if is_cuda :
+            torch.cuda.empty_cache()
 
         return loss_total
 
@@ -64,9 +61,13 @@ class loss(nn.modules.loss._WeightedLoss):
         aux1 = (1 - self.mask)
         aux2 = self.Iout - self.Igt
         l_hole = torch.norm((aux1*aux2), p=1)/Nigt
+        
         del aux1
         del aux2
-        torch.cuda.empty_cache()
+        
+        if is_cuda :
+            torch.cuda.empty_cache()
+            
         return l_hole
 
     def l_valid(self):
@@ -79,21 +80,20 @@ class loss(nn.modules.loss._WeightedLoss):
         Nigt = self.N * B
         aux1 = self.Iout - self.Igt
         l_valid = torch.norm((aux1*self.mask), p=1)/Nigt
+        
         del aux1
-        torch.cuda.empty_cache()
+        
+        if is_cuda : 
+            torch.cuda.empty_cache()
+            
         return l_valid
 
     def l_perc(self):
         """
         Computation of perceptual loss
+        min_img_size = 224
+        The min size, as noted in the PyTorch pretrained models doc, is 224 px.
         :return: value of l_perc
-        """
-        # min_img_size = 224  # The min size, as noted in the PyTorch pretrained models doc, is 224 px.
-
-        """
-        transform_pipeline = transforms.Compose([transforms.ToTensor(),
-                                                 transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                                                      std=[0.229, 0.224, 0.225])])
         """
 
         vgg16 = models.vgg16(pretrained=True).cuda()
@@ -139,7 +139,9 @@ class loss(nn.modules.loss._WeightedLoss):
         del l1_loss2
         del l1_loss1
         self.outputs = []
-        torch.cuda.empty_cache()
+        
+        if is_cuda :
+            torch.cuda.empty_cache()
 
         return l_perc
 
@@ -163,7 +165,9 @@ class loss(nn.modules.loss._WeightedLoss):
         del aux1
         del aux2
         del l_1
-        torch.cuda.empty_cache()
+        
+        if is_cuda :
+            torch.cuda.empty_cache()
 
         return l_style
 
@@ -180,11 +184,13 @@ class loss(nn.modules.loss._WeightedLoss):
             aux2 = torch.matmul(torch.transpose(self.pool_gt[i], 2, 3), self.pool_gt[i])
             l_1 = torch.norm((aux1 - aux2) * Kp, p=1) / (C ** 2)
             l_style += l_1
+            
         del aux1
         del aux2
         del l_1
 
-        torch.cuda.empty_cache()
+        if is_cuda :
+            torch.cuda.empty_cache()
 
         return l_style
 
@@ -196,41 +202,6 @@ class loss(nn.modules.loss._WeightedLoss):
         B, C, H, W = self.Icomp.shape
         Ncomp = B * C * H * W
         
-        """
-        l_tv = 0.0
-        for b in range(B):
-            for c in range(C):
-                hole_pix = (self.mask[b,c,:,:] == 0).nonzero()
-                first_col = self.Icomp[b,c,:,0][:,None]
-                last_col = self.Icomp[b,c,:,-1][:,None]
-                enhanced1 = torch.cat((first_col,self.Icomp[b,c,:,:]),dim=1)
-                enhanced2 = torch.cat((self.Icomp[b,c,:,:], last_col),dim=1)
-                res = enhanced1 - enhanced2
-
-                del enhanced1
-                del enhanced2
-                l1_cols = torch.norm(res[hole_pix[:,0],hole_pix[:,1]], p=1)
-                first_row = self.Icomp[b, c, 0, :][None,:]
-                last_row = self.Icomp[b, c, -1, :][None,:]
-                enhanced1 = torch.cat((first_row, self.Icomp[b,c,:,:]), dim=0)
-                enhanced2 = torch.cat((self.Icomp[b,c,:,:], last_row), dim=0)
-                res = enhanced1 - enhanced2
-                del enhanced1
-                del enhanced2
-                l1_rows = torch.norm(res[hole_pix[:,0],hole_pix[:,1]], p=1)
-                l_tv += (l1_cols + l1_rows)/Ncomp
-
-        del hole_pix
-        del res
-        del l1_rows
-        del l1_cols
-        del first_col
-        del first_row
-        del last_col
-        del last_row
-        torch.cuda.empty_cache()
-        """
-        
         aux1 = self.Icomp[:, :, :, :-1] - self.Icomp[:, :, :, 1:]
         aux2 = self.Icomp[:, :, :-1, :] - self.Icomp[:, :, 1:, :]
         
@@ -238,6 +209,8 @@ class loss(nn.modules.loss._WeightedLoss):
         l_tv = l1_loss/Ncomp
 
         del aux1, aux2, l1_loss
-        torch.cuda.empty_cache()
+        
+        if is_cuda :
+            torch.cuda.empty_cache()
 
         return l_tv
