35c9969d24a6f6fbd3d1652383816acda5f3ee82
Azilyss <49938372+Azilyss@users.noreply.github.com>
2019-05-23 17:12:19 +0200

    Update loss.py

--- loss.py
+++ loss.py
@@ -1,57 +1,72 @@
-import torch
-import torchvision
-from PIL import Image
-from torch import nn
-from torch.autograd import Variable
-from torchvision import models
-from torchvision.transforms import transforms
-import torchvision.models as models
-import numpy as np
-
-
-class loss(nn.Module):
-
-    def __init__(self, img, output, mask):
+class loss(nn.modules.loss._WeightedLoss):
+    def __init__(self):
         super(loss, self).__init__()
         self.C = 3
-        self.H = 512
-        self.W = 512
+        self.H = 256
+        self.W = 256
+        self.N = self.C * self.H * self.W
         self.outputs = []
-        self.Igt = img  # Igt
-        self.Iout = output.data  # Iout torch.nn.Parameter()
-        self.mask = mask  # [None, :, :, :]
-        self.Icomp = self.make_comp()
-        self.N = self.C * self.H * self.W * self.Igt.shape[0]
+        self.Igt = None
+        self.Iout = None
+        self.mask = None
+        self.Icomp = None
+
+    def forward(self, Igt, Iout, mask):
+        self.Igt = Igt
+        self.Iout = Iout
+        self.mask = mask
+        self.make_comp()
+        return self.loss_function()
 
     def make_comp(self):
-        self.Icomp = self.Iout.clone()
-        self.mask[self.mask != 0] = 1
-        dummy = self.mask.clone().long()
-        for b in range(self.Igt.shape[0]):
-            for c in range(self.Igt.shape[1]):
-                self.Icomp[b, c, :, :][np.where(dummy[b, 0, :, :] == 1)] = self.Igt[b, c, :, :][
-                    np.where(dummy[b, 0, :, :] == 1)]
-        return self.Icomp
+        mask = self.mask.clone().type(torch.ByteTensor).cuda()
+        self.Icomp = Variable(torch.where(mask, self.Igt, self.Iout), requires_grad = True)
+
+        """mask = np.ones(X.shape, dtype=bool)
+        mask[:, validation_ind] = 0
+
+        X_train = X[mask].reshape(d*n_len, -1)"""
 
     def loss_function(self):
-        loss = Variable(self.l_hole(), requires_grad=True) + Variable(self.l_valid(), requires_grad=True) + Variable(
-            self.l_perc(), requires_grad=True) + Variable(self.l_style_comp(), requires_grad=True) + Variable(
-            self.l_style_out(), requires_grad=True)
-        return loss
+       
+        loss_total = 6 * self.l_hole() + self.l_valid() + 0.05 * self.l_perc()\
+                    + 120 * (self.l_style_comp() + self.l_style_out()) + 0.1 * self.l_tv()
+        
+        #print("hole : ", self.l_hole())
+        #print("valid : ", self.l_valid())
+        #print("perc : ", self.l_perc())
 
+        #print("style comp : ", self.l_style_comp())
+        #print("style out : ", self.l_style_out())
+
+        #print("tv : ", self.l_tv())
+
+        self.Igt = None
+        self.Iout = None
+        self.Icomp = None
+        self.mask = None
+        self.pool_out = None
+        self.pool_gt = None
+        self.pool_comp = None
+        
+        torch.cuda.empty_cache()
+
+        return loss_total
 
     def l_hole(self):
         """
         Computation of one of the pixel losses: l_hole
         :return: value of l_hole
         """
-        Nigt = self.N
+
+        B, C, W, H = self.Igt.shape
+        Nigt = self.N*B
         aux1 = (1 - self.mask)
         aux2 = self.Iout - self.Igt
-        l1_loss = 0.0
-        for i in range(aux2.shape[0]):
-            l1_loss += torch.norm(aux1 * aux2, p=1)
-        l_hole = l1_loss / Nigt
+        l_hole = torch.norm((aux1*aux2), p=1)/Nigt
+        del aux1
+        del aux2
+        torch.cuda.empty_cache()
         return l_hole
 
     def l_valid(self):
@@ -59,13 +74,13 @@ class loss(nn.Module):
         Computation of one of the pixel losses: l_valid
         :return: value of l_valid
         """
-        Nigt = self.N
+        B, C, W, H = self.Igt.shape
+
+        Nigt = self.N * B
         aux1 = self.Iout - self.Igt
-        l1_loss = 0.0
-        for i in range(self.mask.shape[0]):
-            aux2 = self.mask[i][None, :, :, :] * aux1[i][None, :, :, :]
-            l1_loss += torch.norm(aux2, p=1)
-        l_valid = l1_loss / Nigt
+        l_valid = torch.norm((aux1*self.mask), p=1)/Nigt
+        del aux1
+        torch.cuda.empty_cache()
         return l_valid
 
     def l_perc(self):
@@ -73,39 +88,59 @@ class loss(nn.Module):
         Computation of perceptual loss
         :return: value of l_perc
         """
-        vgg16 = models.vgg16(pretrained=True)
-        min_img_size = 224  # The min size, as noted in the PyTorch pretrained models doc, is 224 px.
+        # min_img_size = 224  # The min size, as noted in the PyTorch pretrained models doc, is 224 px.
+
+        """
         transform_pipeline = transforms.Compose([transforms.ToTensor(),
                                                  transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                                                       std=[0.229, 0.224, 0.225])])
+        """
 
+        vgg16 = models.vgg16(pretrained=True).cuda()
+        for param in vgg16.features.parameters():
+            param.requires_grad = False
         vgg16.features._modules["4"].register_forward_hook(self.hook)
         vgg16.features._modules["9"].register_forward_hook(self.hook)
         vgg16.features._modules["16"].register_forward_hook(self.hook)
 
-        # out
-        img = Variable(self.Iout)
-        out = vgg16.forward(img)
+        self.outputs = []
 
-        # gt
-        img = Variable(self.Igt)
+        img = Variable(self.Iout, requires_grad=False)
         out = vgg16.forward(img)
+        self.pool_out = list(self.outputs)
+
+        self.outputs = []
+
+        img = Variable(self.Igt, requires_grad=False)
+        out = vgg16.forward(img)
+        self.pool_gt = list(self.outputs)
 
         # comp
-        img = Variable(self.Icomp)
-        out = vgg16.forward(img)
+        self.outputs = []
 
-        self.pool_out = self.outputs[0:3].copy()
-        self.pool_gt = self.outputs[3:6].copy()
-        self.pool_comp = self.outputs[6:9].copy()
+        img = Variable(self.Icomp, requires_grad=False)
+        out = vgg16.forward(img)
+        self.pool_comp = list(self.outputs)
+
         l_perc = 0.0
         for i in range(len(self.pool_out)):
-            Nigt = torch.tensor(self.pool_gt[0].shape).prod()
+            Nigt = torch.tensor(self.pool_gt[i].shape).prod()
             diff1 = self.pool_out[i] - self.pool_gt[i]
             l1_loss1 = torch.norm(diff1, p=1)
             diff2 = self.pool_comp[i] - self.pool_gt[i]
             l1_loss2 = torch.norm(diff2, p=1)
-            l_perc += (l1_loss1 + l1_loss2) / Nigt
+            l_perc += (l1_loss1 + l1_loss2)/Nigt
+
+        del img
+        del out
+        del Nigt
+        del diff1
+        del diff2
+        del l1_loss2
+        del l1_loss1
+        self.outputs = []
+        torch.cuda.empty_cache()
+
         return l_perc
 
     def hook(self, module, input, output):
@@ -119,16 +154,17 @@ class loss(nn.Module):
         l_style = 0.0
         for i in range(len(self.pool_gt)):
             B, C, H, W = self.pool_gt[i].shape
-            Kp = 1 / (C * H * W)
-            for b in range(B):
-                for c in range(C):
-                    phi_out_t = torch.transpose(self.pool_out[i][b, c, :, :], 0, 1)
-                    phi_gt_t = torch.transpose(self.pool_gt[i][b, c, :, :], 0, 1)
-                    diff = Kp * (torch.mm(phi_out_t, self.pool_out[i][b, c, :, :]) - torch.mm(phi_gt_t,
-                                                                                              self.pool_gt[i][b, c, :,
-                                                                                              :]))
-                    l1 = torch.norm(diff, p=1)
-                    l_style += l1 / (C * C)
+            Kp = 1.0 / (C * H * W* B)
+            aux1 = torch.matmul(torch.transpose(self.pool_out[i],2,3),self.pool_out[i])
+            aux2 = torch.matmul(torch.transpose(self.pool_gt[i],2,3),self.pool_gt[i])
+            l_1 = torch.norm((aux1-aux2)*Kp, p=1)/(C**2)
+            l_style += l_1
+
+        del aux1
+        del aux2
+        del l_1
+        torch.cuda.empty_cache()
+
         return l_style
 
     def l_style_comp(self):
@@ -139,18 +175,69 @@ class loss(nn.Module):
         l_style = 0.0
         for i in range(len(self.pool_gt)):
             B, C, H, W = self.pool_gt[i].shape
-            Kp = 1 / (C * H * W)
-            for b in range(B):
-                for c in range(C):
-                    phi_comp_t = torch.transpose(self.pool_comp[i][b, c, :, :], 0, 1)
-                    phi_gt_t = torch.transpose(self.pool_gt[i][b, c, :, :], 0, 1)
-                    diff = Kp * (torch.mm(phi_comp_t, self.pool_comp[i][b, c, :, :]) -
-                                 torch.mm(phi_gt_t, self.pool_gt[i][b, c, :, :]))
-                    l1 = torch.norm(diff, p=1)
-                    l_style += l1 / (C * C)
+            Kp = 1.0 / (C * H * W*B)
+            aux1 = torch.matmul(torch.transpose(self.pool_comp[i], 2, 3), self.pool_comp[i])
+            aux2 = torch.matmul(torch.transpose(self.pool_gt[i], 2, 3), self.pool_gt[i])
+            l_1 = torch.norm((aux1 - aux2) * Kp, p=1) / (C ** 2)
+            l_style += l_1
+        del aux1
+        del aux2
+        del l_1
+
+        torch.cuda.empty_cache()
+
         return l_style
 
-# if __name__ == '__main__':
-#     lt = loss_test(Igt, Iout, mask)
-#     lt.img = Image.open('rick_morty.png', 'r')
-#     lt.l_perc()
+    def l_tv(self):
+        """
+        Compute the smoothing penalty: total variation loss
+        :return: total variation value
+        """
+        B, C, H, W = self.Icomp.shape
+        Ncomp = B * C * H * W
+        
+        """
+        l_tv = 0.0
+        for b in range(B):
+            for c in range(C):
+                hole_pix = (self.mask[b,c,:,:] == 0).nonzero()
+                first_col = self.Icomp[b,c,:,0][:,None]
+                last_col = self.Icomp[b,c,:,-1][:,None]
+                enhanced1 = torch.cat((first_col,self.Icomp[b,c,:,:]),dim=1)
+                enhanced2 = torch.cat((self.Icomp[b,c,:,:], last_col),dim=1)
+                res = enhanced1 - enhanced2
+
+                del enhanced1
+                del enhanced2
+                l1_cols = torch.norm(res[hole_pix[:,0],hole_pix[:,1]], p=1)
+                first_row = self.Icomp[b, c, 0, :][None,:]
+                last_row = self.Icomp[b, c, -1, :][None,:]
+                enhanced1 = torch.cat((first_row, self.Icomp[b,c,:,:]), dim=0)
+                enhanced2 = torch.cat((self.Icomp[b,c,:,:], last_row), dim=0)
+                res = enhanced1 - enhanced2
+                del enhanced1
+                del enhanced2
+                l1_rows = torch.norm(res[hole_pix[:,0],hole_pix[:,1]], p=1)
+                l_tv += (l1_cols + l1_rows)/Ncomp
+
+        del hole_pix
+        del res
+        del l1_rows
+        del l1_cols
+        del first_col
+        del first_row
+        del last_col
+        del last_row
+        torch.cuda.empty_cache()
+        """
+        
+        aux1 = self.Icomp[:, :, :, :-1] - self.Icomp[:, :, :, 1:]
+        aux2 = self.Icomp[:, :, :-1, :] - self.Icomp[:, :, 1:, :]
+        
+        l1_loss = torch.norm(aux1[self.mask[:, :, :, :-1] == 1], p=1) + torch.norm(aux2[self.mask[:, :, :-1, :] == 1], p=1)
+        l_tv = l1_loss/Ncomp
+
+        del aux1, aux2, l1_loss
+        torch.cuda.empty_cache()
+
+        return l_tv
