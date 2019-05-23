ee85295bce94f06bb6ce02c2be7b6f05086ca847
Manon Deprette <manon.deprette@gmail.com>
2019-05-23 18:29:28 +0200

    r

--- Programming_Part/metrics.py
+++ Programming_Part/metrics.py
@@ -1,8 +1,6 @@
-from sklearn import metrics
 from skimage import measure
 
 import numpy as np
-import torch
 
 def PSNR(y_true, y_pred):
   MSE = np.mean(np.square(y_pred - y_true), axis = (1,2,3))
@@ -21,16 +19,3 @@ def compute_MSSIM(y_true, y_pred, window_size = 7) :
       MSSIM[i] += measure.compare_ssim(y_true[i,j,:,:], y_pred[i,j,:,:], win_size = window_size)
     MSSIM[i] /= y_true.shape[1] #average over channels
   return MSSIM
-
-if __name__ == '__main__':
-  y_true = torch.rand(10, 3, 512, 512)
-  y_pred = torch.rand(10, 3, 512, 512)
-
-  y_true = y_true.numpy()
-  y_pred = y_pred.numpy()
-
-  print("PSNR : ", PSNR(y_true, y_pred))
-  print("l1 : ", l1(y_true, y_pred))
-
-  window_size = 7
-  print("MSSIM average over nb of channels :", compute_MSSIM(y_true, y_pred, window_size))
